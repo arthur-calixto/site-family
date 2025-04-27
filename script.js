ed1933ba1601d7af24e3d9078ede0968feff10aa
Arthur Calixto <88586876+arthur-calixto@users.noreply.github.com>
2025-04-27 15:42:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ const btnTitia = document.getElementById('btnTitia');
 const surpresa = document.getElementById('surpresa');
 const mensagem = document.getElementById('mensagem');
 const foto = document.getElementById('foto');
+const titulo = document.querySelector('h2'); // Pega o h2 (a mensagem inicial)
 
 btnVovo.addEventListener('click', () => {
   mostrarSurpresa(
     'Vocês serão vovô e vovó este ano!',
-    'foto-vovos.jpeg' 
+    'foto-vovos.jpeg'
   );
 });
 
 btnTitia.addEventListener('click', () => {
   mostrarSurpresa(
     'Você será titia este ano!',
-    'foto-titia.jpeg' 
+    'foto-titia.jpeg'
   );
 });
 
 function mostrarSurpresa(texto, caminhoDaFoto) {
   document.querySelector('.buttons').style.display = 'none';
+  titulo.style.display = 'none'; // <-- Essa linha esconde o h2 também
   mensagem.textContent = texto;
   foto.src = caminhoDaFoto;
   surpresa.classList.remove('hidden');
