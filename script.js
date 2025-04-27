2b5a14b2b6b9c598e15f67ec97e3fe7adc70ea90
Arthur Calixto <88586876+arthur-calixto@users.noreply.github.com>
2025-04-27 15:36:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnTitia = document.getElementById('btnTitia');
 const surpresa = document.getElementById('surpresa');
 const mensagem = document.getElementById('mensagem');
 const foto = document.getElementById('foto');
+const titulo = document.querySelector('h2');
 
 btnVovo.addEventListener('click', () => {
   mostrarSurpresa(
@@ -14,12 +15,13 @@ btnVovo.addEventListener('click', () => {
 btnTitia.addEventListener('click', () => {
   mostrarSurpresa(
     'Você será titia este ano!',
-    'foto-titia.jpeg' 
+    'foto-titia.jpeg'
   );
 });
 
 function mostrarSurpresa(texto, caminhoDaFoto) {
   document.querySelector('.buttons').style.display = 'none';
+  titulo.style.display = 'none'; 
   mensagem.textContent = texto;
   foto.src = caminhoDaFoto;
   surpresa.classList.remove('hidden');
