b1bc91f4969ed73bfb58cf61bc8d7fe4faa33b14
Arthur Calixto <88586876+arthur-calixto@users.noreply.github.com>
2025-05-06 16:36:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const btnVovo = document.getElementById('btnVovo');
 const btnTitia = document.getElementById('btnTitia');
+const btnBerenice = document.getElementById('btnBerenice');
+
 const surpresa = document.getElementById('surpresa');
-const btnBerenice = document.getElementById('btnBerenice'); 
 const mensagem = document.getElementById('mensagem');
 const foto = document.getElementById('foto');
-const titulo = document.querySelector('h2'); // Pega o h2 (a mensagem inicial)
-
+const titulo = document.getElementById('titulo');
 
 btnVovo.addEventListener('click', () => {
   mostrarSurpresa(
@@ -21,21 +21,23 @@ btnTitia.addEventListener('click', () => {
   );
 });
 
-
-
-
 btnBerenice.addEventListener('click', () => {
   mostrarSurpresa(
-    'A senhora vai ser bisavó este ano !',
-    'foto-berenice.jpeg' // nova imagem
+    'Você será titia este ano, Berenice!',
+    'foto-berenice.jpeg'
   );
 });
 
-
 function mostrarSurpresa(texto, caminhoDaFoto) {
   document.querySelector('.buttons').style.display = 'none';
-  titulo.style.display = 'none'; // <-- Essa linha esconde o h2 também
+  titulo.style.display = 'none';
   mensagem.textContent = texto;
+
+  // Reinicia a animação
+  foto.classList.remove('animated');
+  void foto.offsetWidth;
   foto.src = caminhoDaFoto;
+  foto.classList.add('animated');
+
   surpresa.classList.remove('hidden');
 }
